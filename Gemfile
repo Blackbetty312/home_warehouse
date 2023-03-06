332775580fd90dbff5f6dc4d9802f6ef443b8864
Dominik Wawrzynczak <dwawrzynczak@anteeo.com.pl>
2023-03-06 21:38:50 +0100

    favicon, style ekranow devise

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem 'omniauth-rails_csrf_protection', '~> 1.0'
 gem 'will_paginate', '~> 3.3.1', '>= 3.3.1'
 gem 'i18n-active_record', require: 'i18n/active_record'
 gem 'cssbundling-rails', '~> 1.1', '>= 1.1.2'
+gem 'toastr-rails'
 group :development, :test do
 # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
 gem "debug", "1.7.1", platforms: %i[ mri mingw x64_mingw ]
--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -1,13 +1,22 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import 'bootstrap/dist/css/bootstrap.css'
-import * as bootstrap from 'bootstrap'
-// bootstrap;
+import "@hotwired/turbo-rails";
+import "./controllers";
+import "bootstrap/dist/css/bootstrap.css";
+import "@fortawesome/fontawesome-free/js/all";
+import "bootstrap-social";
+import "./controllers/jquery";
+import { bootstrap } from "./controllers/bootstrap";
+import * as toastr from 'toastr';
 (() => {
-    'use strict'
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-      new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-  })()
+  "use strict";
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+})();
+
+
+$(() => {
+});
